d9bd18a9770e4ea2099c6686084ac88a14507e5d
JuanGongora <gongora.animations@gmail.com>
2018-07-04 19:43:29 -0400

    first test for controller output

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Front controller
+ */
+
+//test this out by going to url and setting a delimeter with a value and you'll see string output
+echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
