ff2fc4c0529ed3bbf0d1a50911729bf397eb84c3
JuanGongora <gongora.animations@gmail.com>
2018-07-06 12:10:33 -0400

    inserted getter and add method for Router class

--- Core/Router.php
+++ Core/Router.php
@@ -1,6 +1,27 @@
 <?php
 
-class Router
-{
+class Router {
+
+    //assoc. array of routes (the routing table)
+    protected $routes = [];
+
+    /**
+     * @param $route
+     * @param $params (controller, action, etc.)
+     */
+    public function add($route, $params) {
+
+        $this->routes[$route] = $params;
+    }
+
+    /**
+     * gets all routes from the routing table
+     *
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
 
 }
